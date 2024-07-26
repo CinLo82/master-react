aa1b0ac679afe2b0f52b1841df5c195a9f402afd
CinLo82 <cinlos3382@gmail.com>
2024-07-25 23:43:04 -0300

    use-effect

---
+++ 03-use-effect/src/component/PruebasComponent.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+
+export const PruebasComponent = () => {
+        
+    const [user, setUser] = useState('Cinlo Losada')
+    const [fecha, setFecha] = useState('01-02-1999')
+    const [contador, setContador] = useState(0)
+
+    const modUser = (e) => {
+        setUser(e.target.value)
+    }
+    
+    const cambiarFecha = () => {
+        setFecha(Date.now())
+    }
+
+    useEffect(() => {
+        console.log('Haz cargado el componente PruebasComponent')
+    }, [])
+
+    //Se ejecuta solo si cambio el usuario
+    useEffect(() => {
+        setContador(contador+1)
+        console.log('Haz modificado el usuario '+ contador)
+
+    }, [user, fecha])
+
+
+
+    return (
+        <div>
+            <h1>El efecto - Hook useEffect</h1>
+            <strong className={contador >= 10 ? 'label label-green' : 'label'}>{user}</strong>
+            <strong>{fecha}</strong>
+            <br />
+            <input 
+                className='input'
+                type='text' 
+                onChange={ modUser } 
+                placeholder='change name'
+            />
+            <button onClick={cambiarFecha} >
+                Cambiar fecha
+            </button>
+        </div>
+    )
+}
