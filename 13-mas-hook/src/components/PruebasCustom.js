fb013b3eabb2f9be6f7eacc501990d2d4a693eea
CinLo82 <cinlos3382@gmail.com>
2024-08-04 01:12:43 -0300

    hook personalizado, usando estados

--- 13-mas-hook/src/components/PruebasCustom.js
+++ 13-mas-hook/src/components/PruebasCustom.js
@@ -3,13 +3,15 @@ import { useMayus } from '../hook/useMayus'
 
 export const PruebasCustom = () => {
 
-    const {mayusculas, minusculas, concatenar} = useMayus('Cinlo Losad Web')
-
-    console.log(concatenar('is the better'))
+    const {estado, mayusculas, minusculas, concatenar} = useMayus('Cinlo Losada WEB')
 
     return (
         <div>
             <h1>Probando componentes personalizados</h1>
+            <h2>{estado}</h2>
+            <button onClick={ mayusculas }>Poner en Mayuscula</button>
+            <button onClick={ minusculas }>Poner en Minuscula</button>
+            <button onClick={ e => concatenar('- Probando hook personalizados') }>Concatenar</button>
         </div>
     )
 }
