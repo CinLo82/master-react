f9cec162a60081e837ef3b0b954af72f819f777a
CinLo82 <cinlos3382@gmail.com>
2024-08-09 23:08:24 -0300

    borrar articulos

--- 14-proyecto3/src/components/pages/Listado.jsx
+++ 14-proyecto3/src/components/pages/Listado.jsx
@@ -1,6 +1,18 @@
 import { Global } from "../../helpers/Global"
+import { Peticion } from "../../helpers/Peticion"
 
 export const Listado = ({ articles, setArticles}) => {
+
+    let deleteArticle = async(id) => {
+
+        const { datos } = await Peticion(`${Global.url}/borrar/`+id, "DELETE");
+            if(datos.status === "success"){
+                let articleUpdate = articles.filter(article => article._id !== id)
+                setArticles(articleUpdate)
+            }
+        console.log(datos);
+    }
+
     return (
         articles.map(article => {
             return (
@@ -18,7 +30,12 @@ export const Listado = ({ articles, setArticles}) => {
                         <h3 className="title">{article.title}</h3>
                         <p className="description">{article.content}</p>
                         <button className="edit">Editar</button>
-                        <button className="delete">Eliminar</button>
+                        <button 
+                            className="delete"
+                            onClick={() => {deleteArticle(article._id)}}
+                        >
+                            Eliminar
+                        </button>
                     </div>
                 </article> 
             )  
