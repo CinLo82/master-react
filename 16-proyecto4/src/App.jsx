9cc73a7f56ab2b537bb146a50101e39c3c533e82
Cintia Losada <cinlos3382@gmail.com>
2024-08-25 14:27:20 -0300

    barra de navegación privada

--- 16-proyecto4/src/App.jsx
+++ 16-proyecto4/src/App.jsx
@@ -1,13 +1,10 @@
-
-
+import Header from "./components/layout/general/Header"
 
 function App() {
- 
-
     return (
         <>
             <div className="layout">
-                <h1>Proyecto Red Social</h1>
+                <Header />
             </div>    
         </>
     )
