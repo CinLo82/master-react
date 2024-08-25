a0df547a0fa6ca5aa2f2598ab4774b48f1ec722d
Cintia Losada <cinlos3382@gmail.com>
2024-08-25 02:50:27 -0300

    configurando el proyecto para iniciar front de red social

---
+++ 16-proyecto4/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+ 
+
+    return (
+        <>
+            <div className="layout">
+                <h1>Proyecto Red Social</h1>
+            </div>    
+        </>
+    )
+}
+
+export default App
