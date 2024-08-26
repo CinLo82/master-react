18ebdff74e75f21a70d22899aebc5015adb42c2f
Cintia Losada <cinlos3382@gmail.com>
2024-08-26 10:35:13 -0300

    Registro de usuarios

--- 16-proyecto4/src/components/user/Register.jsx
+++ 16-proyecto4/src/components/user/Register.jsx
@@ -1,102 +1,134 @@
-import { useForm } from "../../hooks/useForm"
+import { useState } from 'react'
+import { Global } from '../../helpers/Global'
+import { useForm } from '../../hooks/useForm'
 
 export const Register = () => {
 
     const { form, handleChange } = useForm({})
+    const [saved, setSaved] = useState('not_sended')
 
-    const saveUser = (e) => {
+
+    const saveUser = async(e) => {
+        //prevenir actualizacion de pantalla
         e.preventDefault()
 
+        // Recoger datos del form
         let newUser = form
-       console.log(newUser)
+
+        //Guardar usuario en el backend
+        try {
+            const request = await fetch(Global.url + 'user/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            })
+            const data = await request.json()
+            if(data.status === 'success') {
+                setSaved('saved')
+            } else {
+                setSaved('error')
+            }
+        } catch (error) {
+            console.log(error)
+            setSaved('error')
+        }
     }
+
     return (
         <> 
-            <header className="content__header content__header--public">
-                <h1 className="content__title">Register</h1>
+            <header className='content__header content__header--public'>
+                <h1 className='content__title'>Register</h1>
             </header>
-            <div className="content__post">
+            <div className='content__post'>
+            {saved === 'saved' ?
+                <strong className='alert alert-success'> Usuario registrado correctamente!!</strong>
+            : ''}
+            {saved === 'error' ?
+                <strong className='alert alert-danger'>Usuario no se ha registrado!!</strong>
+            : ''}
                 <form 
-                    className="register-form"
+                    className='register-form'
                     onSubmit={saveUser}
                 >
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label 
-                            htmlFor="name" 
-                            className="form__label"
-                            >Nombre
+                            htmlFor='name' 
+                            className='form__label'
+                        >
+                            Nombre
                         </label>
                         <input 
-                            type="text" 
-                            name="name" 
-                            id="name" 
+                            type='text' 
+                            name='name' 
+                            id='name' 
                             onChange={handleChange}
                         />
                     </div>
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label 
-                            htmlFor="surname" 
-                            className="form__label"
+                            htmlFor='surname' 
+                            className='form__label'
                         >
                             Apellido
                         </label>
                         <input 
-                            type="text" 
-                            name="surname" 
-                            id="surname" 
+                            type='text' 
+                            name='surname' 
+                            id='surname' 
                             onChange={handleChange}
                         />
                     </div>
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label 
-                            htmlFor="nick" 
-                            className="form__label"
+                            htmlFor='nick' 
+                            className='form__label'
                         >
                             Nick
                         </label>
                         <input 
-                            type="text" 
-                            name="nick" 
-                            id="nick" 
+                            type='text' 
+                            name='nick' 
+                            id='nick' 
                             onChange={handleChange}
                         />
                     </div>
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label 
-                            htmlFor="email" 
-                            className="form__label"
+                            htmlFor='email' 
+                            className='form__label'
                         >
                             Correo Electronico
                         </label>
                         <input 
-                            type="email" 
-                            name="email" 
-                            id="email" 
+                            type='email' 
+                            name='email' 
+                            id='email' 
                             onChange={handleChange}
                         />
                     </div>
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label 
-                            htmlFor="password" 
-                                className="form__label"
-                            >
-                                Contraseña
-                            </label>
+                            htmlFor='password' 
+                            className='form__label'
+                        >
+                            Contraseña
+                        </label>
                         <input 
-                            type="password" 
-                            name="password" 
-                            id="password" 
+                            type='password' 
+                            name='password' 
+                            id='password' 
                             onChange={handleChange} 
                         />
                     </div>
                     <input 
-                        type="submit" 
-                        value="Enviar"  
-                        className="btn btn-success" 
+                        type='submit' 
+                        value='Enviar'  
+                        className='btn btn-success' 
                     />
                 </form>
-
-            </div>
+            </div>  
         </>
     )
 }
