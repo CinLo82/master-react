ad5921f3f9611bb4f1c80db411df161aa5b27a1a
Cintia Losada <cinlos3382@gmail.com>
2024-08-26 18:07:08 -0300

    rutas y enlaces de la barra de navegacion

--- 16-proyecto4/src/components/layout/private/Nav.jsx
+++ 16-proyecto4/src/components/layout/private/Nav.jsx
@@ -1,52 +1,57 @@
 import avatar from '../../../assets/img/user.png'
 import { NavLink } from 'react-router-dom'
+import { useAuth } from '../../../hooks/useAuth'
+import { Global } from '../../../helpers/Global'
 
 export const Nav = () => {
+
+    const { auth } = useAuth()
     return (
         <nav className="navbar__container-lists">
             <ul className="container-lists__menu-list">
                 <li className="menu-list__item">
-                    <a href="#" className="menu-list__link">
+                    <NavLink to='/social/' className="menu-list__link">
                         <i className="fa-solid fa-house"></i>
                         <span className="menu-list__title">Inicio</span>
-                    </a>
+                    </NavLink>
                 </li>
                 <li className="menu-list__item">
-                    <a href="#" className="menu-list__link">
+                    <NavLink to='/social/feed' className="menu-list__link">
                         <i className="fa-solid fa-list"></i>
                         <span className="menu-list__title">Timeline</span>
-                    </a>
+                    </NavLink>
                 </li>
                 <li className="menu-list__item">
-                    <a href="#" className="menu-list__link">
+                    <NavLink to='/social/people' className="menu-list__link">
                         <i className="fa-solid fa-user"></i>
                         <span className="menu-list__title">Gente</span>
-                    </a>
+                    </NavLink>
                 </li>
                 <li className="menu-list__item">
-                    <a href="#" className="menu-list__link">
+                    <NavLink to='/social/' className="menu-list__link">
                         <i className="fa-regular fa-envelope"></i>
                         <span className="menu-list__title">Mensajes</span>
-                    </a>
+                    </NavLink>
                 </li>
             </ul>
     
             <ul className="container-lists__list-end">
                     <li className="list-end__item">
-                        <a href="#" className="list-end__link-image">
-                            <img src={avatar} className="list-end__img" alt="Imagen de perfil" />
-                        </a>
+                        <NavLink to='/social' className="list-end__link-image">
+                        {auth.image !== 'default.png' && <img src={Global.url + 'user/avatar/' + auth.image} className="list-end__img" alt="Foto de perfil" />}
+                            {auth.image === 'default.png' && <img src={avatar} className="list-end__img" alt="Foto de perfil" />}
+                        </NavLink>
                     </li>
                 <li className="list-end__item">
-                <a href="#" className="list-end__link">
-                <span className="list-end__name">Nick</span>
-                </a>
+                <NavLink to='/social' className="list-end__link">
+                <span className="list-end__name">{auth.nick}</span>
+                </NavLink>
                 </li>
                 <li className="list-end__item">
-                <a href="#" className="list-end__link">
+                <NavLink to='/social/ajustes' className="list-end__link">
                     <i className="fa-solid fa-cog"></i>
                     <span className="list-end__name">Ajustes</span>
-                </a>
+                </NavLink>
                 </li>
                 <li className="list-end__item">
                 <NavLink to='/social/logout' className="list-end__link">
