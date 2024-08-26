fb6944b959ec3bfc73365df589fc1adab7bc681f
Cintia Losada <cinlos3382@gmail.com>
2024-08-26 15:53:46 -0300

    restringir el acceso a la parte publica si estoy identificado

--- 16-proyecto4/src/components/layout/public/PublicLayout.jsx
+++ 16-proyecto4/src/components/layout/public/PublicLayout.jsx
@@ -1,7 +1,10 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
 import { Header } from "./Header"
+import { useAuth } from "../../../hooks/useAuth"
 
 export const PublicLayout = () => {
+
+    const { auth } = useAuth()
     return (
         <>
             {/* Layout */}
@@ -9,7 +12,11 @@ export const PublicLayout = () => {
 
             {/* Contenido principal */}
             <section className="layout__content">
+                {!auth._id ?
                 <Outlet />
+                : 
+                <Navigate to="/social" />
+                }
             </section>
         </>
     )
