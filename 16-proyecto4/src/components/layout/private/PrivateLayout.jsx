7d922f6a8516102f65ec1046d801a16ae5b160f3
Cintia Losada <cinlos3382@gmail.com>
2024-08-26 00:07:59 -0300

    rutas anidadas en la parte privada

--- 16-proyecto4/src/components/layout/private/PrivateLayout.jsx
+++ 16-proyecto4/src/components/layout/private/PrivateLayout.jsx
@@ -1,16 +1,21 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
+import { Sidebar } from "./Sidebar"
 
 export const PrivateLayout = () => {
     return (
         <>
-            {/* Layout */}
+            {/* Cabecera y Navegacion */}
             <Header />
 
             {/* Contenido principal */}
             <section className="layout__content">
                 <Outlet />
             </section>
+
+            {/* Barra lateral */}
+            <Sidebar />
+            
         </>
     )
 }
--- 16-proyecto4/src/router/Routing.jsx
+++ 16-proyecto4/src/router/Routing.jsx
@@ -2,6 +2,8 @@ import { Route,  Routes,  Navigate, BrowserRouter } from 'react-router-dom'
 import { PublicLayout } from '../components/layout/public/PublicLayout'
 import { Login } from '../components/user/Login'
 import { Register } from '../components/user/Register'
+import { PrivateLayout } from '../components/layout/private/PrivateLayout'
+import { Feed } from '../components/publication/Feed'
 
 export const Routing = () => {
     return (
@@ -12,6 +14,11 @@ export const Routing = () => {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                 </Route>   
+                <Route path="/social" element={<PrivateLayout />} >
+                    <Route index element={<Feed />} />
+              
+
+                </Route>
             </Routes>
         </BrowserRouter>
     )
